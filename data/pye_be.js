1f1288751d7887d7bd7c94e0b6750dc104b6e89d
Alejandro Lentini <alentini@tenaris.com>
2025-05-07 20:48:51 -0300

    feat: update questions and exams banks

--- data/pye_be.js
+++ data/pye_be.js
@@ -5,5 +5,6 @@
 exams = [
     {id: 'E00', type: 'Cuestionario de evaluación contínua', name: 'Simulación cuestionario de evaluación contínua', date: '09/94/2025', questions: [1002, 1006, 1008, 1013, 1010], questionsPoints: [2, 2, 2, 2, 2], answers: []},
     //{id: 'E4519', type: 'Cuestionario de evaluación contínua', name: 'UT1 - Estadística descriptiva', date: '16/09/2025', questions: [1027, 1035, 1043, 1046, 1062], questionsPoints: [2, 2, 2, 2, 2], answers: []},
-    {id: 'E1703', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '30/04/2025', questions: [2001, 2002, 2003, 2004, 2005], questionsPoints: [2, 2, 2, 2, 2], answers: []}
+    //{id: 'E1703', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '30/04/2025', questions: [2001, 2002, 2003, 2004, 2005], questionsPoints: [2, 2, 2, 2, 2], answers: []}
+    {id: 'E8492', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '07/05/2025', questions: [2006, 2007, 2008, 2009, 2010], questionsPoints: [2, 2, 2, 2, 2], answers: []}
 ]
