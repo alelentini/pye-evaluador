a26b56e35e037fa0ee51a6c2b5cd3472de5ad985
Alejandro Lentini <alentini@tenaris.com>
2025-06-18 20:26:50 -0300

    feat: update questions and exams banks

--- data/pye_be.js
+++ data/pye_be.js
@@ -5,7 +5,8 @@
 exams = [
     {id: 'E00', type: 'Cuestionario de evaluación contínua', name: 'Simulación cuestionario de evaluación contínua', date: '09/94/2025', questions: [1002, 1006, 1008, 1013, 1010], questionsPoints: [2, 2, 2, 2, 2], answers: []},
     //{id: 'E4519', type: 'Cuestionario de evaluación contínua', name: 'UT1 - Estadística descriptiva', date: '16/09/2025', questions: [1027, 1035, 1043, 1046, 1062], questionsPoints: [2, 2, 2, 2, 2], answers: []},
-    //{id: 'E1703', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '30/04/2025', questions: [2001, 2002, 2003, 2004, 2005], questionsPoints: [2, 2, 2, 2, 2], answers: []}
-    //{id: 'E8492', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '07/05/2025', questions: [2006, 2007, 2008, 2009, 2010], questionsPoints: [2, 2, 2, 2, 2], answers: []}
-    {id: 'E0206', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '28/05/2025', questions: [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020], questionsPoints: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], answers: []}
+    //{id: 'E1703', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '30/04/2025', questions: [2001, 2002, 2003, 2004, 2005], questionsPoints: [2, 2, 2, 2, 2], answers: []},
+    //{id: 'E8492', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '07/05/2025', questions: [2006, 2007, 2008, 2009, 2010], questionsPoints: [2, 2, 2, 2, 2], answers: []},
+    //{id: 'E0206', type: 'Cuestionario de evaluación contínua', name: 'UT2 - Probabilidad', date: '28/05/2025', questions: [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020], questionsPoints: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], answers: []},
+    {id: 'E9023', type: 'Cuestionario de evaluación contínua', name: 'UT3 - Distirbuciones de probabilidad discreta', date: '18/06/2025', questions: [3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010], questionsPoints: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], answers: []}
 ]
